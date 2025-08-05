fe883624a7120f300589090b08e6ff992df9dc15
quentinkhoo <quentinkhoo@outlook.com>
2025-08-05 20:51:21 +0800

    phonebot

--- phonebot.py
+++ phonebot.py
@@ -2,6 +2,7 @@
 from dotenv import load_dotenv
 import discord
 import os
+from datetime import datetime
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -12,7 +13,6 @@ TEST_USER_ID = 210092278675537920
 
 # #dota2 news feed
 LILBITCH_CHANNEL_ID = 861425569887682620
-EXTRA_PEACEFUL_CHANNEL_ID = 865560755138986014
 # our favourite
 PHONE_USER_ID = 150641779794903040
 
@@ -32,17 +32,16 @@ async def on_presence_update(old_presence, new_presence):
 
   test_channel = client.get_channel(TEST_CHANNEL_ID)
   lilbitch_channel = client.get_channel(LILBITCH_CHANNEL_ID)
-  extra_peaceful_channel = client.get_channel(EXTRA_PEACEFUL_CHANNEL_ID)
     
-  if (new_presence.id == PHONE_USER_ID):
+  if (new_presence.id == TEST_USER_ID):
     if (new_presence.activity != None and new_presence.activity.name == "Dota 2"):
+      print(f"Phone started playing at {datetime.now()}")
       await test_channel.send('@everyone phone is online on dota boys!')
-      #await lilbitch_channel.send('@everyone phone is online on dota boys!')
-      #await extra_peaceful_channel.send('@everyone phone is online on dota boys!')
+      await lilbitch_channel.send('@everyone phone is online on dota boys!')
 
-    if (new_presence.activity == None and old_presence.activity.name == "Dota 2"):
+    if (old_presence.activity and old_presence.activity.name == "Dota 2"):
+      print(f"Phone stopped playing at {datetime.now()}")
       await test_channel.send('@everyone phone stop playing le, can finally win')
-      #await lilbitch_channel.send('@everyone phone stop playing le, can finally win')
-      #await extra_peaceful_channel.send('@everyone phone stop playing le, can finally win')
+      await lilbitch_channel.send('@everyone phone stop playing le, can finally win')
 
 client.run(BOT_TOKEN)
