7b3263c042fd6f729a4227e319191dbaecb381ad
quentinkhoo <quentinkhoo@outlook.com>
2025-08-05 21:28:47 +0800

    fix

--- phonebot.py
+++ phonebot.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import discord
 import os
 from datetime import datetime
+import time
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -11,10 +12,11 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 TEST_CHANNEL_ID = 1401921828804493353
 TEST_USER_ID = 210092278675537920
 
-# #dota2 news feed
-LILBITCH_CHANNEL_ID = 861425569887682620
 # our favourite
 PHONE_USER_ID = 150641779794903040
+
+GUILDS = ["Lilbitch", "EXTRA-PEACEFULL", "staging"]
+GUILD_CHANNEL_IDS = [861425569887682620, 865560755138986014, TEST_CHANNEL_ID]
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -30,18 +32,15 @@ async def on_ready():
 @client.event
 async def on_presence_update(old_presence, new_presence):
 
-  test_channel = client.get_channel(TEST_CHANNEL_ID)
-  lilbitch_channel = client.get_channel(LILBITCH_CHANNEL_ID)
+  channel = client.get_channel(GUILD_CHANNEL_IDS[GUILDS.index(old_presence.guild.name)])
     
   if (new_presence.id == TEST_USER_ID):
     if (new_presence.activity != None and new_presence.activity.name == "Dota 2"):
       print(f"Phone started playing at {datetime.now()}")
-      await test_channel.send('@everyone phone is online on dota boys!')
-      await lilbitch_channel.send('@everyone phone is online on dota boys!')
+      await channel.send("@everyone hello boys, come dotes don't dodge")
 
     if (old_presence.activity and old_presence.activity.name == "Dota 2"):
       print(f"Phone stopped playing at {datetime.now()}")
-      await test_channel.send('@everyone phone stop playing le, can finally win')
-      await lilbitch_channel.send('@everyone phone stop playing le, can finally win')
+      await channel.send("@everyone i stop le, jiahui can turn off stream le")
 
 client.run(BOT_TOKEN)
